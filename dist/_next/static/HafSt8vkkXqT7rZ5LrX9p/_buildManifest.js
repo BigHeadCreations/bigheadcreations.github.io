2d7082bae8d146773ffa9aa9c50f092876e48568
Timothy Pearson <timo.555@icloud.com>
2022-03-06 16:29:38 +0000

    initial commit of nextjs static site

---
+++ dist/_next/static/HafSt8vkkXqT7rZ5LrX9p/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-132e2dffc6a39ba2.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],"/post/[id]":["static/chunks/pages/post/[id]-6e4d3238a9a5f7a2.js"],sortedPages:["/","/_app","/_error","/post/[id]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
